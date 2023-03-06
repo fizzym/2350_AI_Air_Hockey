ba17b697d584d9f730a36040eb43121d6732a4b1
BradyChurch <bradychurch9@gmail.com>
2023-03-05 18:04:41 -0800

    Got env rendering working. Need code cleanup

--- air_hockey_gym/air_hockey_gym/envs/air_hockey_v0.py
+++ air_hockey_gym/air_hockey_gym/envs/air_hockey_v0.py
@@ -1,11 +1,14 @@
 from gymnasium.envs.mujoco import MujocoEnv
 from gymnasium.spaces import Box
+import os
+import mujoco
+import mediapy as media
 
 import numpy as np
 
 DEFAULT_CAMERA_CONFIG = {
     "trackbodyid": 0,
-    "distance": -1,
+    "distance": 2.4,
 }
 
 #TODO see if I need to add EzPickle
@@ -25,9 +28,23 @@ class AirHockeyEnv(MujocoEnv):
 
         #TODO define max speeds and positions
         observation_space = Box(low=-np.inf, high=np.inf, shape=(12,), dtype=np.float64)
+        self.asset_path = "/home/bradychurch/Desktop/459_ai_air_hockey_gym/air_hockey_gym/air_hockey_gym/envs/assets/"
+
+        asset_names = []
+        for file in os.listdir(self.asset_path):
+        	if file.endswith(".stl"):
+        		asset_names.append(file)
+
+        print(asset_names)
+        self.assets = {}
+        for file in asset_names:
+            with open(self.asset_path+file, 'rb') as f:
+                self.assets[file] = f.read()
+
+        print(self.assets.keys())
         MujocoEnv.__init__(
             self,
-            "/home/bradychurch/Desktop/air_hockey_gym/air_hockey_gym/envs/assets/air_hockey.xml",
+            self.asset_path + "air_hockey.xml",
             #TODO check if I need to change frame skip
             2,
             observation_space=observation_space,
@@ -35,6 +52,33 @@ class AirHockeyEnv(MujocoEnv):
             #default_camera_config=DEFAULT_CAMERA_CONFIG,
             **kwargs,
         )
+
+        #self._initialize_simulation()
+
+    def _initialize_simulation(self):
+        print("In air hockey _initialize_simulation")
+
+        xml = """
+		<mujoco>
+		  <worldbody>
+		    <geom name="red_box" type="box" size=".2 .2 .2" rgba="1 0 0 1"/>
+		    <geom name="green_sphere" pos=".2 .2 .2" size=".1" rgba="0 1 0 1"/>
+		  </worldbody>
+		</mujoco>
+		"""
+        self.model = mujoco.MjModel.from_xml_path(self.fullpath, self.assets)
+        print(self.model.nu)
+    	#self.model = mujoco.MjModel.from_xml_string(xml)
+    	# MjrContext will copy model.vis.global_.off* to con.off*
+        self.model.vis.global_.offwidth = self.width
+        self.model.vis.global_.offheight = self.height
+        self.data = mujoco.MjData(self.model)
+        
+
+        renderer = mujoco.Renderer(self.model)
+        mujoco.mj_forward(self.model, self.data)
+        renderer.update_scene(self.data)
+        media.write_image(self.asset_path + "test_image.png", renderer.render())
 
     #@param a - action
     def step(self, a):
--- air_hockey_test.py
+++ air_hockey_test.py
@@ -1,16 +1,13 @@
 import gymnasium as gym
-#import air_hockey_gym
 from air_hockey_gym.envs import AirHockeyEnv
 import time
 
 env = AirHockeyEnv(render_mode="human")
 
-for i in range(0,100):
+for i in range(0,1000):
 	env.render()
-	time.sleep(0.1)
-#env.render()
-#gym.make("AirHockey-v0", render_mode="human")
-#observation, info = env.reset()
+	env.step([])
+
 
 
 env.close()
