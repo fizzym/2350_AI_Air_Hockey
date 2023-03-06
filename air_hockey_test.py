6fbf4f0e42cc263c197e181e747f7c883ada66ff
BradyChurch <bradychurch9@gmail.com>
2023-03-05 18:54:32 -0800

    Added range of actions inputs to test file

--- air_hockey_test.py
+++ air_hockey_test.py
@@ -4,10 +4,18 @@ import time
 
 env = AirHockeyEnv(render_mode="human")
 
+action = [5,0]
+print(env.action_space)
 for i in range(0,1000):
 	env.render()
-	#env.step([])
-	time.sleep(0.01)
+	if i == 250:
+		action = [-5,0]
+	if i == 500:
+		action = [0,5]
+	if i == 750:
+		action = [0,-5]
+		
+	env.step(action)
 
 
 
