ab7c4836938c985a0f4a7720d9bec9f8833659f0
BradyChurch <bradychurch9@gmail.com>
2023-03-09 19:52:33 -0800

    Added documentation for observations and actions and changed observation space

--- air_hockey_gym/air_hockey_gym/envs/air_hockey_v0.py
+++ air_hockey_gym/air_hockey_gym/envs/air_hockey_v0.py
@@ -1,5 +1,5 @@
 from gymnasium.envs.mujoco import MujocoEnv
-from gymnasium.spaces import Box
+from gymnasium.spaces import Box, Dict
 import os
 import mujoco
 
@@ -13,6 +13,63 @@ DEFAULT_CAMERA_CONFIG = {
 #TODO see if I need to add EzPickle
 class AirHockeyEnv(MujocoEnv):
 
+	"""
+
+	### Mallet Definition
+	Mallet 1 - Mallet that plays in -x direction from table coordinate frame.
+	The mallet on the left side of the table when looking from default camera angle. 
+	
+
+	Mallet 2 - Mallet that plays in +x direction from table coordinate frame.
+	The mallet on the right side of the table when looking from default camera angle.
+
+	### Observation 
+	Dictionary of mallet name to (3,4) numpy array containing positions and velocities of all objects (puck, mallet 1, mallet 2)
+
+	Keys:
+	mal_1 - Mallet 1
+	mal_2 - Mallet 2
+
+	Values:
+	(3,4) np array (obs)
+	Each row of the array represents [x position, y position, x velocity, y velocity] for a given object.
+
+	Coordinates are in reference frame of controlled mallet (mallet that corresponds to dict key used). 
+	Mallet reference frame is defined with origin at centre of table, x-y plane in the plane of the table,
+	and the positive x-axis pointing away from the controlled mallet's goal. This is done so observation
+	is independent of which side of the table the mallet is on. 
+
+	Row 0 = Puck
+	Row 1 = Controlled Mallet
+	Row 2 = Opponent Mallet
+
+	Ex. obs[0, 2] = x position of the puck in the controlled mallet's reference frame
+
+
+	### Action
+	(4,) numpy array (act) containing accelerations of both mallet's
+
+	act[:2] = [x1 acceleration, y1 accleration] acceleration of Mallet 1 in Mallet 1 reference frame
+	act[2:] = [x2 acceleration, y2 accleration] acceleration of Mallet 2 in Mallet 2 reference frame  
+
+	### Reward
+	Dict of mallet name to reward (int)
+
+	Keys:
+	mal_1 - Mallet 1 
+	mal_2 - Mallet 2 
+
+	Value:
+	#TODO describe reward definition
+
+
+	### Episode End
+	A training episode terminates when the centre point of the puck passes the goal line in either goal
+
+
+	
+
+	"""
     metadata = {
         #All 3 render modes required for MujocoEnv
         "render_modes": [
@@ -26,10 +83,18 @@ class AirHockeyEnv(MujocoEnv):
         "render_fps": 25,
     }
 
+
+
     def __init__(self, **kwargs):
 
-        #TODO define max speeds and positions
-        observation_space = Box(low=-np.inf, high=np.inf, shape=(12,), dtype=np.float64)
+    	#Name of agents used as keys in dictionaries
+        self.mal1_name = "mal_1"
+        self.mal2_name = "mal_2"
+
+        #TODO define max speeds and position
+        observation_space = Dict({self.mal1_name: Box(low=-np.inf, high=np.inf, shape=(3,4), dtype=np.float64),
+        self.mal2_name: Box(low=-np.inf, high=np.inf, shape=(3,4), dtype=np.float64)})
+        
         self.asset_path = os.path.join(os.path.dirname(__file__), "assets/")
         print(self.asset_path)
 
@@ -82,4 +147,6 @@ class AirHockeyEnv(MujocoEnv):
     def _get_obs(self):
 
         #TODO access observation from self.data
-        return np.ones((12,), np.float64)
+        array1 = np.ones((3,4), np.float64)
+        array2 = np.zeros((3,4), np.float64)
+        return {self.mal1_name: array1, self.mal2_name : array2}
