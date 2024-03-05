28ed677a2cfa01e0b1f3b5d64f58fa380348e904
bchurch8 <67076325+bchurch8@users.noreply.github.com>
2024-03-04 19:57:15 -0800

    Brady/adv off test (#27)
    * Added offensive data collection script
    
    * Address PR comments
    
    * Added offensive training data
    
    * Create advanced data collection script and start modifications
    
    * Finalized initial data collection script
    
    * Add new test files
    
    * Initial implementation complete
    
    * Fix bugs
    
    * Fix multi-agent bugs in base class
    
    * Add data folder
    
    * Add comments to data collection script
    
    * Add some args and comments to adv_def_test
    
    * Modify tensorboard message
    
    * Modify agent controlled mallet spawn location
    
    * Add adv offensive data generation notebook and initial data
    
    * Add adv offensive test and config
    
    * Update config file with data options
    
    * Fix typo
    
    ---------
    
    Co-authored-by: Brian Sun <briansun997@gmail.com>

---
+++ agent_validation/adv_offence_val_test_v0.py
@@ -0,0 +1,61 @@
+from agent_validation.val_test import ValidationTest
+from agents.rl_agent import RL_Agent
+from air_hockey_gym.envs.single_mallet_shooting_v2 import SingleMalletShootEnvV2
+from torch.utils.tensorboard import SummaryWriter 
+import pickle 
+import numpy as np
+
+
+class AdvOffenceValTest(ValidationTest):
+    
+    def __init__(self, **kwargs):
+        """Perform any required initialization.
+        """
+        pass
+
+    def test_agent(self, agent : RL_Agent, log_path : str, render_mode, data_name : str,
+                   discrete_actions = True, **kwargs):
+        """Perform desired test. Saves relevant statistics using tensorboard to log_path.
+
+        Args:
+            agent: The agent to test.
+            log_path: Directory to save test logs to.
+            render_mode: How to render environment used during testing. Options are 'human', 'rgb_array', 'depth_array' 
+            data_name: Name of the recorded testing data to use. Should be in agent_validation/data directory
+        """
+
+        env = SingleMalletShootEnvV2(render_mode = render_mode, discrete_actions = discrete_actions)
+        tb = SummaryWriter(log_path)
+
+        testing_data = []
+
+        with open("agent_validation/data/" + data_name, "rb") as file:
+            testing_data = pickle.load(file)
+
+        num_goals = 0
+        for k in range(len(testing_data)):
+
+            #Grab data for current episode and reset environment to recorded ICs
+            obs = env.set_custom_state(testing_data[k], np.zeros((6,1)))
+
+            terminated = False
+
+            #For each episode run through recorded actions + buffer
+            while not terminated:
+                action = agent.predict(obs)
+
+                obs, rew, terminated, _, info_dict = env.step(action)
+
+                if terminated:
+                    if info_dict["termination_reason"] == "Goal scored on opponent":
+                        num_goals += 1
+
+                    #Reset model so step counter is also reset
+                    env.reset()
+            
+            
+
+        tb.add_scalar("Advanced Offence Test - Goals Scored", num_goals, 0)
+        print("Agent scored", num_goals, "times")          
+
+        tb.close()
